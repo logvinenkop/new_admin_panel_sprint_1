d4fbc72c67da1867f5821eff943e37053d56f5e3
Pavel Logvinenko <logvinenkop@gmail.com>
2022-04-04 17:11:20 +0300

    Добавлен django-debug-toolbar

--- 02_movies_admin/config/settings.py
+++ 02_movies_admin/config/settings.py
@@ -39,6 +39,10 @@ DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -49,6 +53,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "movies.apps.MoviesConfig",
 ]
 
@@ -60,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -125,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
